e35fcb16d653e5bf7b75feeb32c24034016c35b3
KSenia2011KSenia2011 <ksenia.stancyk@gmail.com>
2025-05-21 20:29:33 +0300

    1

---
+++ Lesson1/1.py
@@ -0,0 +1,4 @@
+print ("Hello world")
+print(" /\_/\\\n( o.o )\n > ^ <\n")
+print("ʕ•ᴥ•ʔ\n(___)\n U U\n")
+print("         z\n       z\n        z\n      z\n /\_/\\\n(>^.^<)\n \/ \/")
---
+++ Lesson4/4.py
@@ -0,0 +1,10 @@
+name = "Ксенія"
+number = 10
+is_number = True
+
+print(is_number)
+print(number+100)
+
+x=input("Введіть щось сюди:")
+print("Дякую! Ви ввели:")
+print(x)
